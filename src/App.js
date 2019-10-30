85382db17d3620d3583d13d96ceac086c90c2135
victorh3 <victorh3@live.com>
2019-10-29 22:22:20 -0500

    Create project scaffolding

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Testimonials from './components/Testimonials';
+import Portfolio from './components/Portfolio';
+import me from './components/resumeData.json';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {},
+    };
+  }
+
+  getResumeData() {
+    // $.ajax({
+    //   url: '/resumeData.json',
+    //   dataType: 'json',
+    //   cache: false,
+    //   success: function(data) {
+    this.setState({ resumeData: me });
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     console.log(err);
+    //     alert(err);
+    //   },
+    // });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Testimonials data={this.state.resumeData.testimonials} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
+      </div>
+    );
+  }
+}
+
+export default App;
